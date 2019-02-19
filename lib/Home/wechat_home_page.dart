64eff8d1ea0957988349745e08519b28c44a5e34
chenxiaolong <chenxiaolong@bignox.com>
2019-02-19 21:53:01 +0800

    添加图标

--- lib/Home/wechat_home_page.dart
+++ lib/Home/wechat_home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import '../Common/wechat_constant.dart' show WechatColors;
+import '../Common/wechat_constant.dart' show WechatColors,WechatIcons;
 // import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+enum WechatHomePopmenuAction {
+  HOME_CHAT_GROUP,HOME_ADD_FRIEND,HOME_SACN,HOME_PAYMENT
+}
 
 class WechatHomePage extends StatefulWidget {
   _WechatHomePageState createState() => _WechatHomePageState();
@@ -24,34 +28,51 @@ class _WechatHomePageState extends State<WechatHomePage> {
         title: Text("微信"),
         centerTitle: true,
         actions: <Widget>[
-          // IconButton(
-          //   icon: Icon(Icons.search),
-          //   onPressed: (){},
-          // ),
            PopupMenuButton(
             icon: Icon(Icons.add),
             offset: Offset(0, 60),
             itemBuilder:(BuildContext context){
-              return <PopupMenuEntry<String>>[
+              return <PopupMenuEntry<WechatHomePopmenuAction>>[
                   PopupMenuItem(
                       child:  Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
-                          Icon(Icons.add_circle),
-                          Text('增加')
+                          Icon(IconData(0xe601,fontFamily: WechatIcons.WechatIconFontFamily)),
+                          Text('开始群聊')
                         ],
                       ),
-                      value: '这是增加',
+                      value: WechatHomePopmenuAction.HOME_CHAT_GROUP,
                     ),
                     PopupMenuItem(
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
-                          Icon(Icons.remove_circle),
-                          Text('减少')
+                          Icon(IconData(0xe6ce,fontFamily: WechatIcons.WechatIconFontFamily)),
+                          Text('添加好友')
                         ],
                       ),
-                      value: '这是删除',
+                      value: WechatHomePopmenuAction.HOME_ADD_FRIEND,
+                    ) ,
+                    PopupMenuItem(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Icon(IconData(0xe68a,fontFamily: WechatIcons.WechatIconFontFamily)),
+                          Text('扫一扫')
+                        ],
+                      ),
+                      value: WechatHomePopmenuAction.HOME_SACN,
+                    ),
+                    PopupMenuItem(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Icon(IconData(0xe658,fontFamily: WechatIcons.WechatIconFontFamily)),
+                          Text('收付款')
+                        ],
+                      ),
+                      value: WechatHomePopmenuAction.HOME_PAYMENT,
                     )
               ];
             } ,
