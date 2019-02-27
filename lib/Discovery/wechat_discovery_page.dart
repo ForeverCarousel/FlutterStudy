81dc314d76f4548452d95679a8a9c97a165e7dc2
chenxiaolong <chenxiaolong@bignox.com>
2019-02-27 21:44:37 +0800

    添加部分测试代码,这个版本暂时无法运行

--- lib/Discovery/wechat_discovery_page.dart
+++ lib/Discovery/wechat_discovery_page.dart
@@ -11,9 +11,53 @@ class WechatDiscoveryPage extends StatelessWidget {
         elevation: 0.0, //取消bar底部material风格的滚动标示图产生的阴影
         centerTitle: true,
       ),
+      body: MyFadeTest(title: "测试",),
+    );
+  }
+}
+
+class MyFadeTest extends StatefulWidget {
+  MyFadeTest({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyFadeTest createState() => _MyFadeTest();
+}
+
+class _MyFadeTest extends State<MyFadeTest> with TickerProviderStateMixin {
+  AnimationController controller;
+  CurvedAnimation curve;
+
+  @override
+  void initState() {
+    controller = AnimationController(duration: const Duration(milliseconds: 2000), vsync: this);
+    curve = CurvedAnimation(parent: controller, curve: Curves.easeInOut);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Center(
-        child: Text("这是发现页，待开发"),
+        child: FadeTransition(
+            opacity: curve,
+            child: FlutterLogo(size: 200.0)
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Fade',
+        child: Icon(Icons.brush),
+        onPressed: () {
+          controller.forward();
+        },
       ),
     );
+  }
+
+  @override
+  dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
--- lib/Me/wechat_profile_page.dart
+++ lib/Me/wechat_profile_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/Common/wechat_constant.dart' show WechatColors;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 
 class WechatProfilePage extends StatelessWidget {
   @override
@@ -15,5 +18,53 @@ class WechatProfilePage extends StatelessWidget {
         child: Text("这是个人页，待开发"),
       ),
     );
+  }
+}
+
+
+//ttttttt
+class SampleAppPage extends StatefulWidget {
+  SampleAppPage({Key key}) : super(key: key);
+
+  @override
+  _SampleAppPageState createState() => _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+  List widgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sample App"),
+      ),
+      body: ListView.builder(
+          itemCount: widgets.length,
+          itemBuilder: (BuildContext context, int position) {
+            return getRow(position);
+          }));
+  }
+
+  Widget getRow(int i) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Text("Row ${widgets[i]["title"]}")
+    );
+  }
+
+  loadData() async {
+    String dataURL = "https://jsonplaceholder.typicode.com/posts";
+    http.Response response = await http.get(dataURL);
+    setState(() {
+      widgets = json.decode(response.body);
+    });
   }
 }
